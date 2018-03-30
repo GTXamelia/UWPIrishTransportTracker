b2d56ff8b74b6fd35f2841f70e0041ee912629b1
Cian Gannon <lego@live.ie>
2018-03-30 15:34:18 +0100

    Added more MainMenu.xaml.cs comments

--- IrishBusStopTracker/MainMenu.xaml.cs
+++ IrishBusStopTracker/MainMenu.xaml.cs
@@ -15,24 +15,28 @@ namespace IrishBusStopTracker
 		// Button fuction for when user presses 'Add_Transport' button in xaml
 		private void Add_Transport(object sender, RoutedEventArgs e)
 		{
+			// Moves user to AddTransport.xaml page
 			this.Frame.Navigate(typeof(AddTransport));
 		}
 
 		// Button fuction for when user presses 'View_Busses' button in xaml
 		private void View_Busses(object sender, RoutedEventArgs e)
 		{
+			// Moves user to BusTransport.xaml page
 			this.Frame.Navigate(typeof(BusTransport));
 		}
 
 		// Button fuction for when user presses 'View_Trains' button in xaml
 		private void View_Trains(object sender, RoutedEventArgs e)
 		{
+			// Moves user to TrainMenu.xaml page
 			this.Frame.Navigate(typeof(TrainMenu));
 		}
 
 		// Button fuction for when user presses 'View_Luas' button in xaml
 		private void View_Luas(object sender, RoutedEventArgs e)
 		{
+			// Moves user to LuasMenu.xaml page
 			this.Frame.Navigate(typeof(LuasMenu));
 		}
 	}
